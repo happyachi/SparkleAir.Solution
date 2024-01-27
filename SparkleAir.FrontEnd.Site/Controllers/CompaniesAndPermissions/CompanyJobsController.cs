2355bba624b559308ab9916a1c21763f9c7b0c0f
happyachi <gn77233@gmail.com>
2024-01-28 00:11:55 +0800

    CompanyJob 基本CRUD

--- SparkleAir.FrontEnd.Site/Controllers/CompaniesAndPermissions/CompanyJobsController.cs
+++ SparkleAir.FrontEnd.Site/Controllers/CompaniesAndPermissions/CompanyJobsController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using SparkleAir.BLL.Service.CompanyAndPermission;
+using SparkleAir.DAL.EFRepository.CompanyAndPermission;
+using SparkleAir.IDAL.IRepository.CompanyAndPermission;
+using SparkleAir.Infa.Dto.CompanyAndPermission;
+using SparkleAir.Infa.ViewModel.CompanyAndPermission;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +13,130 @@ namespace SparkleAir.FrontEnd.Site.Controllers.CompaniesAndPermissions
 {
     public class CompanyJobsController : Controller
     {
+        private readonly CompanyJobService _service;
+        private readonly ICompanyJobRepository _repo;
+
+        public CompanyJobsController()
+        {
+            _repo = new CompanyJobEFRepository();
+            _service = new CompanyJobService(_repo);
+        }
+
         // GET: CompanyJobs
         public ActionResult Index()
         {
+            var dto = _service.Search();
+            var vm = dto.Select(c => new CompanyJobIndexVm
+            {
+                Id = c.Id,
+                CompanyDepartmentId = c.CompanyDepartmentId,
+                CompanyDepartmentName = c.CompanyDepartmentName,
+                JobTitle = c.JobTitle,
+                Rank = c.Rank
+            }).ToList();
+
+            return View(vm);
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CompanyJobIndexVm vm)
+        {
+            if (!ModelState.IsValid) return View(vm);
+
+            try
+            {
+                var dto = new CompanyJobDto()
+                {
+                    CompanyDepartmentId = vm.CompanyDepartmentId,
+                    CompanyDepartmentName = vm.CompanyDepartmentName,
+                    JobTitle = vm.JobTitle,
+                    Rank = vm.Rank
+                };
+
+                _service.Create(dto);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(vm);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (!ModelState.IsValid) return RedirectToAction("Index");
+
+            try
+            {
+                var dto = _service.Get(id);
+
+                var vm = new CompanyJobIndexVm
+                {
+                    Id = dto.Id,
+                    CompanyDepartmentId = dto.CompanyDepartmentId,
+                    CompanyDepartmentName = dto.CompanyDepartmentName,
+                    JobTitle = dto.JobTitle,
+                    Rank = dto.Rank
+                };
+
+                return View(vm);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CompanyJobIndexVm vm)
+        {
+            if (!ModelState.IsValid) return View(vm);
+
+            try
+            {
+                var dto = new CompanyJobDto
+                {
+                    Id = vm.Id,
+                    CompanyDepartmentId = vm.CompanyDepartmentId,
+                    CompanyDepartmentName = vm.CompanyDepartmentName,
+                    JobTitle = vm.JobTitle,
+                    Rank = vm.Rank
+                };
+
+                _service.Update(dto);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(vm);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (!ModelState.IsValid) return RedirectToAction("Index");
+
+            try
+            {
+                _service.Delete(id);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return RedirectToAction("Index");
+            }
         }
     }
 }
--- SparkleAir.IDAL.IRepository/CompanyAndPermission/ICompanyJobRepository.cs
+++ SparkleAir.IDAL.IRepository/CompanyAndPermission/ICompanyJobRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SparkleAir.Infa.Entity.CompanyAndPermission;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace SparkleAir.IDAL.IRepository.CompanyAndPermission
 {
     public interface ICompanyJobRepository
     {
+        List<CompanyJobEntity> Search();
+
+        void Create(CompanyJobEntity entity);
+
+        CompanyJobEntity Get(int id);
+
+        void Update(CompanyJobEntity entity);
+
+        void Delete(int id);
     }
 }
