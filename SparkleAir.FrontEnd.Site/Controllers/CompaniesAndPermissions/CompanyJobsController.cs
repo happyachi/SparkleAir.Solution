22bcd36224b0ee6d6556f38d75ee118d31257848
happyachi <gn77233@gmail.com>
2024-01-27 21:28:45 +0800

    CompanyDepartment 基本CRUD

---
+++ SparkleAir.FrontEnd.Site/Controllers/CompaniesAndPermissions/CompanyJobsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SparkleAir.FrontEnd.Site.Controllers.CompaniesAndPermissions
+{
+    public class CompanyJobsController : Controller
+    {
+        // GET: CompanyJobs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SparkleAir.IDAL.IRepository/CompanyAndPermission/ICompanyDepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using SparkleAir.Infa.Entity.CompanyAndPermission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkleAir.IDAL.IRepository.CompanyAndPermission
+{
+    public interface ICompanyDepartmentRepository
+    {
+        List<CompanyDepartmentEntity> Search ();
+
+        void Create(CompanyDepartmentEntity department);
+
+        CompanyDepartmentEntity Get(int id);
+
+        void Update(CompanyDepartmentEntity department);
+
+        void Delete(int id);
+    }
+}
