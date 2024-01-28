86dc521d6514ae64b2c35a70cf2b800a20a891cf
happyachi <gn77233@gmail.com>
2024-01-28 18:35:12 +0800

    CompanyStaff 基本CRUD

--- SparkleAir.Infa.Criteria/CompanyAndPermission/CompanyStaffGetCriteria.cs
+++ SparkleAir.Infa.Criteria/CompanyAndPermission/CompanyStaffGetCriteria.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SparkleAir.Infa.Criteria.Members
+namespace SparkleAir.Infa.Criteria.CompanyAndPermission
 {
-    public class MemberGetCriteria
+    public class CompanyStaffGetCriteria
     {
         public int? Id { get; set; }
+
         public string Account { get; set; }
 
         public string Password { get; set; }
---
+++ SparkleAir.Infa.ViewModel/CompanyAndPermission/CompanyStaffIndexVm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkleAir.Infa.ViewModel.CompanyAndPermission
+{
+    public class CompanyStaffIndexVm
+    {
+        public int Id { get; set; }
+
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int CompanyJobId { get; set; }
+
+        public string CompanyDepartmentName { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime RegistrationTime { get; set; }
+    }
+}
