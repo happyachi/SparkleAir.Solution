77d7bb9bd2bb7a7c6c40908b5af4fc1a36aefff8
Cheerscheer <chiya951@gmail.com>
2024-01-29 09:57:53 +0800

    新增資料傳輸檔案

---
+++ SparkleAir.Infa.ViewModel/TaxFree/TFItemVm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkleAir.Infa.ViewModel.TaxFree
+{
+    public class TFItemEntity
+    {
+        public int Id { get; set; }
+
+        public int TFCategoriesId { get; set; }
+
+        public string Name { get; set; }
+
+
+        public string SerialNumber { get; set; }
+
+        public string Image { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int UnitPrice { get; set; }
+
+
+        public string Description { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public int TFCategoryId { get; set; }
+        public virtual TFCategororyEntity TFCategory { get; set; }
+
+    }
+
+    public class TFCategororyEntity
+    {
+        public int Id { get; set; }
+
+        public string Category { get; set; }
+
+    }
+}
