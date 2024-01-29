185c1c09d096771e1174683265b59363efd0feec
WHC3 <jim3090666@gmail.com>
2024-01-29 13:26:53 +0800

    GET、Delete、Create、Edit

---
+++ SparkleAir.Infa.Entity/Luggages/LuggageEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkleAir.Infa.Entity.Luggage
+{
+    public class LuggageEntity
+    {
+        public int Id { get; set; }
+
+        public int AirFlightManagementsId { get; set; }
+
+        public int OriginalPrice { get; set; }
+
+        public int BookPrice { get; set; }
+    }
+}
