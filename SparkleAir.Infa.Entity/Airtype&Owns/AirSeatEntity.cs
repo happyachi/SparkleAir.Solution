669ed142556db48cdc230b4c6325080a5b8f6938
helloworld0606 <ajue660@gmail.com>
2024-01-29 10:32:56 +0800

    airseat-getall

---
+++ SparkleAir.Infa.Entity/Airtype&Owns/AirSeatEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkleAir.Infa.Entity.Airtype_Owns
+{
+    public  class AirSeatEntity
+    {
+        public int Id { get; set; }
+
+        public int AirTypeId { get; set; }
+
+        public int AirCabinId { get; set; }
+
+        public int? SeatNum { get; set; }
+    }
+}
