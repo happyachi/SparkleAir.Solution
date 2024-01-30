48a8f12ee7926aaa85bb05713572ff3bebdde9d8
shu0shuo <155411561+shu0shuo@users.noreply.github.com>
2024-01-30 16:23:48 +0800

    AirMealEFRepository CRUD

---
+++ SparkleAir.IDAL.IRepository/MealMessages/IAirMealRepository.cs
@@ -0,0 +1,24 @@
+﻿using SparkleAir.Infa.Dto.MealMessages;
+using SparkleAir.Infa.Entity.MealMessageEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkleAir.IDAL.IRepository.MealMessages
+{
+    public interface IAirMealRepository
+    {
+        //CRUD
+
+        //頁面輸入完後要傳回資料庫
+        //新增entity類別
+        int Create(AirMealEntity entity);
+        void Update(AirMealEntity entity);
+        void Delete(int id);
+        List<AirMealEntity> Search(string name);
+        //給其他方法用(?
+        AirMealEntity Get (int id);
+    }
+}
