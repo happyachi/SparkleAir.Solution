cdf21829fa1dabf348b840dd941eb28c019e1f90
shu0shuo <155411561+shu0shuo@users.noreply.github.com>
2024-01-31 16:42:20 +0800

    add MessageEFRepo

---
+++ SparkleAir.DAL.EFRepository/MealMessages/MessageEFRepository.cs
@@ -0,0 +1,33 @@
+﻿using SparkleAir.IDAL.IRepository.MealMessages;
+using SparkleAir.Infa.Entity.MealMessageEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkleAir.DAL.EFRepository.MealMessages
+{
+    public class MessageEFRepository : IMessageRepository
+    {
+        public int Create(MessageEntity message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MessageEntity> GetByBoxId(int messageBodId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MessageEntity> SearchByContent(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MessageEntity> SearchByTime(string time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SparkleAir.Infa.Entity/MealMessageEntity/MessageBoxEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkleAir.Infa.Entity.MealMessageEntity
+{
+    public class MessageBoxEntity
+    {
+        public int Id { get; set; }
+
+        public int MemberId { get; set; }
+
+        public bool IsProcessed { get; set; }
+
+        public DateTime? ProcessedTime { get; set; }
+    }
+}
