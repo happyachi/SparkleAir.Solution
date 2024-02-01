56cf18b18b9f947afe3ea04d995937ae936dd020
helloworld0606 <ajue660@gmail.com>
2024-02-01 15:49:05 +0800

    [V ] airtypes done( inclu. view) [on working] airowns view( esp. index)

--- SparkleAir.FrontEnd.Site/Controllers/BaseController.cs
+++ SparkleAir.FrontEnd.Site/Controllers/BaseController.cs
@@ -58,6 +58,12 @@ namespace SparkleAir.FrontEnd.Site.Controllers
                         Name = "班次管理",
                         ActionName = "Index",
                         ControllerName = "AirFlights",
+                    },
+                     new MenuItem
+                    {
+                        Name = "機隊管理",
+                        ActionName = "Index",
+                        ControllerName = "Owns",
                     }
                 }
             });
