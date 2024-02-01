d6d95545e02ff0ebd633983bbac15e631d167749
shu0shuo <155411561+shu0shuo@users.noreply.github.com>
2024-02-01 23:03:04 +0800

    Update AirMealService.cs

--- SparkleAir.BLL.Service/MealMessages/AirMealService.cs
+++ SparkleAir.BLL.Service/MealMessages/AirMealService.cs
@@ -1,4 +1,7 @@
 ﻿
+using SparkleAir.IDAL.IRepository.MealMessages;
+using SparkleAir.Infa.Dto.MealMessages;
+using SparkleAir.Infa.Entity.MealMessageEntity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,51 @@ using System.Threading.Tasks;
 
 namespace SparkleAir.BLL.Service.MealMessages
 {
-    internal class AirMealService
+    public class AirMealService
     {
+        private readonly IAirMealRepository _repo;
+        public AirMealService(IAirMealRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public int Create(AirMealDto dto)
+        {
+            AirMealEntity entity = new AirMealEntity
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                AirCabinId = dto.AirCabinId,
+                MealContent = dto.MealContent,
+                Image = dto.Image,
+                ImageBit = dto.ImageBit,
+                Category = dto.Category
+            };
+            _repo.Create(entity);
+            return entity.Id;
+        }
+
+        public void Delete(int id)
+        {
+            _repo.Delete(id);
+        }
+
+        //todo問一下GPT
+        public List<AirMealDto> Search(string name)
+        {
+            var entities=_repo.Search(name);
+            var dtos=entities.Select(x=>new AirMealDto
+            {
+                Id = x.Id,
+                Name = x.Name,
+                AirCabinId=x.AirCabinId,
+                MealContent = x.MealContent,
+                Image = x.Image,
+                ImageBit =x.ImageBit,
+                Category = x.Category
+            }).ToList();
+            return dtos;
+    
+        }
     }
 }
