2f9be15a127e638ae047f51797d79b8144dd11d5
shu0shuo <155411561+shu0shuo@users.noreply.github.com>
2024-02-03 12:56:07 +0800

    變更資料庫

--- SparkleAir.Infa.Utility/Helper/UploadHelper.cs
+++ SparkleAir.Infa.Utility/Helper/UploadHelper.cs
@@ -10,13 +10,14 @@ namespace SparkleAir.Infa.Utility.Helper
 {
     public class UploadHelper
     {
+         
         public string UploadImageFile(HttpPostedFileBase file, string path)
         {
             //判斷有無上傳檔案，沒有則丟出例外
             if (file == null || file.ContentLength == 0) throw new ArgumentNullException("file");
             //取得檔名並判斷是否為允許的檔案類型
 
-            string[] allowExts = { ".jpg", ".jpeg", "png" };
+            string[] allowExts = { ".jpg", ".jpeg", ".png" };
             string ext = Path.GetExtension(file.FileName).ToLower();
             if (!allowExts.Contains(ext)) throw new ArgumentException($"不允許上傳此類檔案({ext})");
             //為避免不同時間上傳相同檔名，每次要取唯一檔名
